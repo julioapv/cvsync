9ca90c778c341cb03a40fff25c4669eb15640570
julio <juliopa54@gmail.com>
2025-02-16 15:01:50 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>CVsync</h1>
+      <p>Quick tool to modern CVs</p>
+      <button>Try now</button>
+    </>
+  )
+}
+
+export default App
